745f7e0254045017fd4389bf06a0979c2d353665
lev.cherepanov <lev.cherepanov2001@yandex.ru>
2025-04-13 03:19:12 +0500

    message

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,9 @@
+/**
+ * Author: Cherepanov lev
+ * Contact: lev.cherepanov2001@yandex.ru
+ * Date: 13.04.2025
+ * Данный код для тестового задания компании Rocont
+ */
 import '@/assets/styles/main.scss'
 
 import { createApp } from 'vue'
